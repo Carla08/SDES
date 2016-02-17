228339f3146434925c8982061e54fb38b17778ba
Carla08 <dcfc@live.com.mx>
2016-02-16 19:08:32 -0700

    Split method

--- main.c
+++ main.c
@@ -9,19 +9,37 @@ int iP[] ={2 ,6 ,3 ,1 ,4 ,8 ,5 ,7};
 int inverIP[] ={4 ,1 ,3 ,5 ,7 ,2 ,8 ,6};
 int ep[] ={4 ,1 ,2 ,3 ,2 ,3 ,4 ,1};
 
-// string[] permutate(String [] p, int length, String text){
+//string[] permutate(String [] p, int length, String text){
 //
-// }
+//}
 
+//LEFT SHIFT
 const char * leftShift(char text[], int shiftValue){
   char * shiftedText = malloc(sizeof(char)*5);
-  int i;
+  short i;
   for (i = 0; i < 5; i++) {
     shiftedText[i%5] = text[(i+shiftValue)%5];
   }
   // shiftedText[i] = '\0';
   return shiftedText;
 }
+
+//SPLIT
+const char * split(char text[],bool keep_left, size_t strLenght) {
+  char * half_array = malloc(sizeof(char)*(strLenght/2));
+  short i;
+  if(keep_left){
+    for (i=0; i<strLenght/2; i++){
+      half_array[i] = text[i];
+    }
+  } else {
+    for (i=strLenght/2; i<strLenght/2; i++){
+      half_array[i] = text[i];
+    }
+  }
+  return half_array;
+}
+//MAIN
 int main(){
 		printf("The shiftedTex -> %s \n",leftShift("10000",2));
 		return 0;
